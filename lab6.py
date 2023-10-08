9b6b7dd39cf81f94e850ae42a98f4f772b6165e1
malikimn <144468336+malikimn@users.noreply.github.com>
2023-10-08 11:26:56 -0400

    Create lab6.py

---
+++ lab6.py
@@ -0,0 +1,9 @@
+def main ():
+    name=input("Enter your name: ")
+    age=int(input("How old will you turn this year? "))
+    year=2023
+    yob=year-num
+    print(f"Hi {name}! You were born in {yob}")
+
+if __name__== "__main__":
+    main()
