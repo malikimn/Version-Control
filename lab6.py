3f5b0db6955b17081f0d8b7c5698b99203b03f4e
malikimn <144468336+malikimn@users.noreply.github.com>
2023-10-08 15:21:14 +0000

    Finished the Program

---
+++ lab6.py
@@ -0,0 +1,6 @@
+def main ():
+    name=input("Enter your name: ")
+    age=int(input("How old will you turn this year? "))
+    year=2023
+    yob=year-num
+    print(f"Hi {name}! You were born in {yob}")
